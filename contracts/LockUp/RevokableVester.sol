e7a67d9e172a1b263df391c94c6e212c0581fc2a
Will <will@deversifi.com>
2022-03-09 14:32:16 +0000

    Add RevokableVester

--- contracts/LockUp/RevokableVester.sol
+++ contracts/LockUp/RevokableVester.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "../Interfaces/IStakedDVF.sol";
 
 /**
@@ -13,11 +14,12 @@ import "../Interfaces/IStakedDVF.sol";
  * @dev A token holder contract that can release its token balance gradually like a
  * typical vesting scheme, with a cliff and vesting period.
  */
-contract SupporterVester {
+contract RevokableVester is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
     event TokensReleased(address token, uint256 amount);
+    event TokenVestingRevoked(address token);
 
     // beneficiary of tokens after they are released
     address private _beneficiary;
@@ -37,6 +39,7 @@ contract SupporterVester {
     uint256 private _duration;
 
     uint256 private _released;
+    bool private _revoked;
 
     /**
      * @dev Creates a vesting contract that vests its balance of any ERC20 token to the
@@ -179,5 +182,32 @@ contract SupporterVester {
 
     function nominateDelegate(address _newDelegate) onlyBeneficiary public {
         _delegateForVoting = _newDelegate;
+    }
+
+    /**
+    * @return true if the token is revoked.
+    */
+    function revoked() public view returns (bool) {
+        return _revoked;
+    }
+
+    /**
+    * @notice Allows the owner to revoke the vesting. Tokens already vested
+    * remain in the contract, the rest are returned to the owner.
+    * @param token ERC20 token which is being vested
+    */
+    function revoke(IERC20 token) public onlyOwner {
+        require(!_revoked, "Vesting: already revoked");
+
+        uint256 balance = token.balanceOf(address(this));
+
+        uint256 unreleased = _releasableAmount();
+        uint256 refund = balance.sub(unreleased);
+
+        _revoked = true;
+
+        token.safeTransfer(owner(), refund);
+
+        emit TokenVestingRevoked(address(token));
     }
 }
